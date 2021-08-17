64a20e9037e6d4caa8c971290d5cd92a99b47f35
Battulapraveenkumar <praveenkumarbattula8@gmail.com>
2021-08-17 21:32:35 +0530

    Testcase1

--- src/test/java/Area_and_PerimeterTest.java
+++ src/test/java/Area_and_PerimeterTest.java
@@ -15,28 +15,5 @@ public class Area_and_PerimeterTest {
         assertEquals(expectedPerimeter,actualPerimeter);
 
     }
-    @Test
-    void ToReturnAreaandPerimeteroftenandtwenty()
-    {
-        Area_and_Perimeter obj = new Area_and_Perimeter(10,20);
-        int expectedArea = 200;
-        int expectedPerimeter = 60;
-        int actualArea = obj.Area();
-        int actualPerimeter = obj.Perimeter();
-        assertEquals(expectedArea,actualArea);
-        assertEquals(expectedPerimeter,actualPerimeter);
 
-    }
-    @Test
-    void ToReturnAreaandPerimeterofminustenandminustwenty()
-    {
-        Area_and_Perimeter obj = new Area_and_Perimeter(-10,-20);
-        int expectedArea = 200;
-        int expectedPerimeter = -60;
-        int actualArea = obj.Area();
-        int actualPerimeter = obj.Perimeter();
-        assertEquals(expectedArea,actualArea);
-        assertEquals(expectedPerimeter,actualPerimeter);
-
-    }
 }
