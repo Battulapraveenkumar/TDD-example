179410cd0b54f9ed92446e916e6e1f76119308b7
Battulapraveenkumar <praveenkumarbattula8@gmail.com>
2021-08-17 21:35:58 +0530

    Testcase2

--- src/test/java/Area_and_PerimeterTest.java
+++ src/test/java/Area_and_PerimeterTest.java
@@ -3,17 +3,17 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Area_and_PerimeterTest {
+
     @Test
-    void ToReturnAreaandPerimeterofFourandSix()
+    void ToReturnAreaandPerimeterofminustenandminustwenty()
     {
-        Area_and_Perimeter obj = new Area_and_Perimeter(4,6);
-        int expectedArea = 24;
-        int expectedPerimeter = 20;
+        Area_and_Perimeter obj = new Area_and_Perimeter(-10,-20);
+        int expectedArea = 200;
+        int expectedPerimeter = -60;
         int actualArea = obj.Area();
         int actualPerimeter = obj.Perimeter();
         assertEquals(expectedArea,actualArea);
         assertEquals(expectedPerimeter,actualPerimeter);
 
     }
-
 }
